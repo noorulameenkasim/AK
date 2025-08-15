45d2913749130654f5333bab1d5400fa6c51471b
AK <ameenkasim26@gmail.com>
2025-08-15 23:34:24 +0530

    chore: scaffold Womigo backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Womigo backend running');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
+
+module.exports = app;
